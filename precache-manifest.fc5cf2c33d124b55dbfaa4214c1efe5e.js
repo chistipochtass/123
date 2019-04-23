6a2e7070a5c74516edcb04b2f1146db8821c42d3
gaserd <gaserd123@gmail.com>
2019-04-23 20:28:43 +0300

    Updates

--- precache-manifest.fc5cf2c33d124b55dbfaa4214c1efe5e.js
+++ precache-manifest.fc5cf2c33d124b55dbfaa4214c1efe5e.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3163b97add201905459ca7dd83f3a807",
+    "revision": "ff83f15aab856db39ef6eca4fa57eb23",
     "url": "/vk-apps-todo/index.html"
   },
   {
-    "revision": "8a68fce83dc42fc97b9f",
+    "revision": "817ca2e96634360c89fb",
     "url": "/vk-apps-todo/static/css/2.b5769064.chunk.css"
   },
   {
-    "revision": "d1051410c97751bcf2ba",
+    "revision": "156b78a3239ff50e8a0b",
     "url": "/vk-apps-todo/static/css/main.c793b830.chunk.css"
   },
   {
-    "revision": "8a68fce83dc42fc97b9f",
-    "url": "/vk-apps-todo/static/js/2.0273cd07.chunk.js"
+    "revision": "817ca2e96634360c89fb",
+    "url": "/vk-apps-todo/static/js/2.0fda37f2.chunk.js"
   },
   {
-    "revision": "d1051410c97751bcf2ba",
-    "url": "/vk-apps-todo/static/js/main.6977ac81.chunk.js"
+    "revision": "156b78a3239ff50e8a0b",
+    "url": "/vk-apps-todo/static/js/main.521cce11.chunk.js"
   },
   {
     "revision": "fb973330fbb88c3fc5db",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/vk-apps-todo/precache-manifest.040eb899ba8477dacf19096755f3a81e.js"
+  "/vk-apps-todo/precache-manifest.fc5cf2c33d124b55dbfaa4214c1efe5e.js"
 );
 
 self.addEventListener('message', (event) => {
